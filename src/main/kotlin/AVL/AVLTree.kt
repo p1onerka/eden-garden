77856c9196bbee5cde3c5fa68ba6243065d5e523
Sofya Kozyreva <k81sofia@gmail.com>
2024-03-19 19:48:49 +0300

    implement balancing after insertion

--- src/main/kotlin/AVL/AVLTree.kt
+++ src/main/kotlin/AVL/AVLTree.kt
@@ -12,7 +12,12 @@ class AVLTree<K : Comparable<K>, V>: balancedTree<K, V, AVLNode<K, V>>() {
     }
     override fun insert(key: K, value: V) {
         val insertedNode = insertNode(key, value)
-        if (insertedNode != null) balanceAfterInsert(insertedNode)
+        if (insertedNode != null) {
+            findParent(insertedNode)?.let {
+                //updateHeight(it) // надо еще подумать, насколько это должно быть тут
+                balanceAfterInsert(it)
+            }
+        }
     }
     private fun getHeight(node: AVLNode<K, V>?): Int {
         return node?.height ?: 0
@@ -23,11 +28,48 @@ class AVLTree<K : Comparable<K>, V>: balancedTree<K, V, AVLNode<K, V>>() {
     private fun getBalanceFactor(node: AVLNode<K, V>): Int {
         return getHeight(node.rightChild) - getHeight(node.leftChild)
     }
-    override fun balanceAfterInsert(insertedNode: AVLNode<K, V>) {
-
-        TODO("Not yet implemented") //посравнивать бф и понять, гнать цикл или как
+    override fun balanceAfterInsert(curNode: AVLNode<K, V>) {
+        when (getBalanceFactor(curNode)) {
+            -2 -> {
+                curNode.leftChild?.let { if (getBalanceFactor(it) == 1) rotateLeft(it) }
+                rotateRight(curNode)
+            }
+            2 -> {
+                curNode.rightChild?.let { if (getBalanceFactor(it) == -1) rotateRight(it) }
+                rotateLeft(curNode)
+            }
+            else -> updateHeight(curNode)
+        }
+        findParent(curNode)?.let { balanceAfterInsert(it) }
     }
     override fun balanceAfterDelete(deletedNode: AVLNode<K, V>) {
         TODO("Not yet implemented")
     }
+    private fun findParent(node: AVLNode<K, V>): AVLNode<K, V>? { // вродь работает
+        var curNode = root
+        var parentNode = root
+        while (curNode != null && curNode.key != node.key) {
+            parentNode = curNode
+            curNode = when {
+                node.key > curNode.key -> curNode.rightChild
+                node.key < curNode.key -> curNode.rightChild
+                else -> break
+            }
+        }
+        //println(parentNode?.key)
+        return null
+    }
+
+}
+fun main() {
+    var tree = AVLTree<Int, Any>()
+    tree.insert(4, "hi")
+    tree.insert(2, "bye")
+    tree.insert(6, "xo")
+    tree.insert(5, "del me")
+    tree.insert(7, "suck")
+    tree.insert(8, "suck")
+    //tree.findParent(7)
+    //tree.printNode(6)
+    //tree.findNodeByKey(4)
 }
