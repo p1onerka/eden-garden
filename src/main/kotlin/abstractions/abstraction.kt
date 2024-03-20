ecc05295464ca6def64f986433021299660c6314
Sofya Kozyreva <k81sofia@gmail.com>
2024-03-21 00:22:10 +0300

    abstractTree: add an exception in delete method

--- src/main/kotlin/abstractions/abstraction.kt
+++ src/main/kotlin/abstractions/abstraction.kt
@@ -52,8 +52,12 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
     }
 
     open fun delete(key: K) {
+        deleteNode(key)
+    }
+
+    protected fun deleteNode(key: K): someNode? {
         val nodeToDelete = findNodeByKey(key)
-        if ((nodeToDelete == null) || (root == null)) return
+        if ((nodeToDelete == null) || (root == null)) return null
         val parentNode = findParent(nodeToDelete)
         /* no children case */
         if (nodeToDelete.leftChild == null && nodeToDelete.rightChild == null)
@@ -66,13 +70,13 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
         /* 2 children case */
         else {
             val replacementNode = findMinNodeInRight(nodeToDelete.rightChild)
-            if (replacementNode != null) {
-                moveParentNode(replacementNode, findParent(replacementNode), null)
-                replacementNode.leftChild = nodeToDelete.leftChild
-                replacementNode.rightChild = nodeToDelete.rightChild
-                moveParentNode(nodeToDelete, findParent(nodeToDelete), replacementNode)
-            }
+                ?: throw IllegalStateException ("Node with 2 children must have a right child")
+            moveParentNode(replacementNode, findParent(replacementNode), null)
+            replacementNode.leftChild = nodeToDelete.leftChild
+            replacementNode.rightChild = nodeToDelete.rightChild
+            moveParentNode(nodeToDelete, parentNode, replacementNode)
         }
+        return parentNode
     }
 
     protected fun findParent(node: someNode): someNode? { // вродь работает
@@ -105,17 +109,17 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
         while (true) {
             minNode = minNode?.leftChild ?: break
         }
-        if (minNode != null) {
-            return minNode
+        when {
+            (minNode != null) -> return minNode
+            else -> return null
         }
-        return subtree
     }
 
     fun find(key: K): V? {
         return findNodeByKey(key)?.value
     }
 
-    protected fun findNodeByKey(key: K): someNode? {
+    private fun findNodeByKey(key: K): someNode? {
         var curNode: someNode? = root ?: return null
         while (curNode != null) {
             curNode = when {
@@ -134,6 +138,18 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
         else {
             println("no such node")
         }
+    }
+
+    fun preOrder(): List<someNode> {
+        val result = mutableListOf<someNode>()
+        fun walk(node: someNode, lst: MutableList<someNode>) {
+            lst.add(node)
+            node.leftChild?.let { walk(it, lst) }
+            node.rightChild?.let { walk(it, lst) }
+        }
+        if (root == null) return result
+        root?.let { walk(it, result) }
+        return result
     }
 }
 
@@ -163,18 +179,18 @@ class BSTree<K : Comparable<K>, V> : abstractTree<K, V, BSNode<K, V>>() {
 
 fun main() {
     var tree = BSTree<Int, Any>()
-    tree.insert(10, "hi")
-    tree.insert(5, "hi")
-    tree.insert(12, "hi")
-    tree.insert(1, "hi")
+//    tree.insert(10, "hi")
+//    tree.insert(5, "hi")
+//    tree.insert(12, "hi")
+//    tree.insert(1, "hi")
     tree.insert(8, "hi")
     //tree.insert(7, "hi")
     tree.delete(8)
     tree.printNode(8)
-//    val myList = tree.preOrder()
-//    for (item in myList) {
-//        print("${item.key} ")
-//    }
+    val myList = tree.preOrder()
+    for (item in myList) {
+        print("${item.key} ")
+    }
 //    tree.insert(4, "hi")
 //    tree.insert(2, "bye")
 //    tree.insert(6, "xo")
