a7d65bd931799054af8931f133cd126c68e4ba58
Sofya Kozyreva <k81sofia@gmail.com>
2024-03-20 15:12:44 +0300

    fix delete method

--- src/main/kotlin/abstractions/abstraction.kt
+++ src/main/kotlin/abstractions/abstraction.kt
@@ -1,5 +1,7 @@
 package abstractions
 
+import AVL.AVLNode
+
 abstract class abstractNode<K: Comparable<K>, V, someNode: abstractNode<K, V, someNode>>(var key:K, var value: V) {
     var leftChild: someNode? = null
     var rightChild: someNode? = null
@@ -8,13 +10,14 @@ abstract class abstractNode<K: Comparable<K>, V, someNode: abstractNode<K, V, so
 class BSNode<K : Comparable<K>, V>(key: K, value: V) : abstractNode<K, V, BSNode<K, V>>(key, value)
 
 abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, someNode>> {
-    var root: someNode? = null
+    private var root: someNode? = null
 
     protected abstract fun createNewNode(key: K, value: V): someNode
 
     open fun insert(key: K, value: V) {
         insertNode(key, value)
     }
+
     protected fun insertNode(key: K, value: V): someNode? {
         val nodeToInsert = createNewNode(key, value)
         var curNode = root
@@ -52,32 +55,49 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
 
     open fun delete(key: K) {
         val nodeToDelete = findNodeByKey(key)
-        if ((nodeToDelete == null) || (root == null)) {
-            println("nothing to delete")
-            return
+        if ((nodeToDelete == null) || (root == null)) return
+        val parentNode = findParent(nodeToDelete)
+        /* no children case */
+        if (nodeToDelete.leftChild == null && nodeToDelete.rightChild == null) moveParentNode(nodeToDelete, parentNode, null)
+        /* 1 child case */
+        else if (nodeToDelete.leftChild == null || nodeToDelete.rightChild == null) {
+            if (nodeToDelete.leftChild == null) moveParentNode(nodeToDelete, parentNode, nodeToDelete.rightChild)
+            else moveParentNode(nodeToDelete, parentNode, nodeToDelete.leftChild)
         }
-        root = deleteRecursively(root, nodeToDelete)
-
-    }
-
-    protected fun deleteRecursively(curNode: someNode?, nodeToDelete: someNode): someNode? {
-        when {
-            curNode == null -> return null
-            curNode.key < nodeToDelete.key -> curNode.rightChild = deleteRecursively(curNode.rightChild, nodeToDelete)
-            curNode.key > nodeToDelete.key -> curNode.leftChild = deleteRecursively(curNode.leftChild, nodeToDelete)
-            else -> {
-                if ((curNode.leftChild == null)||(curNode.rightChild == null)) {
-                    return if (curNode.leftChild != null) curNode.leftChild else curNode.rightChild
-                }
-                val replacementNode = findMinNodeInRight(curNode.rightChild)
-                if (replacementNode != null) {
-                    curNode.key = replacementNode.key
-                    curNode.value = replacementNode.value
-                    curNode.rightChild = deleteRecursively(curNode.rightChild, replacementNode)
-                }
+        /* 2 children case */
+        else {
+            val replacementNode = findMinNodeInRight(nodeToDelete.rightChild)
+            if (replacementNode != null) {
+                moveParentNode(replacementNode, findParent(replacementNode), null)
+                replacementNode.leftChild = nodeToDelete.leftChild
+                replacementNode.rightChild = nodeToDelete.rightChild
+                moveParentNode(nodeToDelete, findParent(nodeToDelete), replacementNode)
             }
         }
-        return curNode
+    }
+
+    protected fun findParent(node: someNode): someNode? { // вродь работает
+        var curNode = root
+        while (curNode != null) {
+            if (curNode.key == node.key) return null
+            if (curNode.leftChild?.key == node.key || curNode.rightChild?.key == node.key) return curNode
+            curNode = when {
+                curNode.key < node.key -> curNode.rightChild
+                else -> curNode.leftChild
+            }
+        }
+        return null
+    }
+    /* moves parent of a node to point to a different node instead */
+    protected fun moveParentNode(node: someNode, parentNode: someNode?, replacementNode: someNode?) {
+        when (parentNode) {
+            null -> if (root == node) root = replacementNode
+            //else throw IllegalArgumentException("Node's parent cannot be null if it isn't a root")
+            else -> {
+                if (parentNode.rightChild == node) parentNode.rightChild = replacementNode
+                else if (parentNode.leftChild == node) parentNode.leftChild = replacementNode
+            }
+        }
     }
 
     private fun findMinNodeInRight(subtree: someNode?): someNode? {
@@ -115,30 +135,21 @@ abstract class abstractTree<K: Comparable<K>, V, someNode: abstractNode<K, V, so
 
 abstract class balancedTree<K: Comparable<K>, V, someNode: abstractNode<K, V, someNode>>: abstractTree<K, V, someNode>() {
     protected abstract fun balanceAfterInsert(curNode: someNode)
+
     protected abstract fun balanceAfterDelete(curNode: someNode)
+
     protected open fun rotateRight (node: someNode, parentNode:  someNode?) {
         val tempNode = node.leftChild ?: throw IllegalArgumentException("Node must have left child for right rotation")
         node.leftChild = tempNode.rightChild
         tempNode.rightChild = node
-        swapNodes(node, parentNode, tempNode)
+        moveParentNode(node, parentNode, tempNode)
     }
+
     protected open fun rotateLeft (node: someNode, parentNode:  someNode?) {
         val tempNode = node.rightChild ?: throw IllegalArgumentException("Node must have right child for left rotation")
         node.rightChild = tempNode.leftChild
         tempNode.leftChild = node
-        swapNodes(node, parentNode,tempNode)
-    }
-    private fun swapNodes(node: someNode, parentNode: someNode?, nodeToSwap: someNode?) {
-        when (parentNode) {
-            null -> {
-                if (root == node) root = nodeToSwap
-                //else throw IllegalArgumentException("Node's parent cannot be null if it isn't a root")
-            }
-            else -> {
-                if (parentNode.rightChild == node) parentNode.rightChild = nodeToSwap
-                else if (parentNode.leftChild == node) parentNode.leftChild = nodeToSwap
-            }
-        }
+        moveParentNode(node, parentNode,tempNode)
     }
 }
 
@@ -147,8 +158,19 @@ class BSTree<K : Comparable<K>, V> : abstractTree<K, V, BSNode<K, V>>() {
 }
 
 fun main() {
-
-//    var tree = BSTree<Int, Any>()
+    var tree = BSTree<Int, Any>()
+    tree.insert(10, "hi")
+    tree.insert(5, "hi")
+    tree.insert(12, "hi")
+    tree.insert(1, "hi")
+    tree.insert(8, "hi")
+    //tree.insert(7, "hi")
+    tree.delete(8)
+    tree.printNode(8)
+//    val myList = tree.preOrder()
+//    for (item in myList) {
+//        print("${item.key} ")
+//    }
 //    tree.insert(4, "hi")
 //    tree.insert(2, "bye")
 //    tree.insert(6, "xo")
