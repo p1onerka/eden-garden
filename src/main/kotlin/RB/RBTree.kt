da5742d94fc8ad990c6dbb66d6711359c00dd41f
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-03-24 14:35:12 +0300

    sonya's rb changes

--- src/main/kotlin/RB/RBTree.kt
+++ src/main/kotlin/RB/RBTree.kt
@@ -15,42 +15,25 @@ class RBTree<K : Comparable<K>, V>: balancedTree<K, V, RBNode<K, V>>() {
     override fun createNewNode(key: K, value: V): RBNode<K, V> {
         return RBNode(key, value)
     }
-//    fun getColor(node: RBNode<K, V>): RBNode<K, V>? {
-//        return if (node.color == C) {
-//            node.parent?.rightChild
-//        } else {
-//            node.parent?.leftChild
-//        }
-//    }
 
-//    fun setColor(newColor: Color) {
-//
-//    }
     override fun insert(key: K, value: V) {
         val newNode = insertNode(key, value)
-
-        if (newNode == null) {
-            //println("no insert have been found")
-            return
-        }
         if (root == newNode) {
-            newNode.color = Color.BLACK
+            root = newNode
+            newNode?.color = Color.BLACK
             return
         }
 
-        val parent = findParent(newNode)
-            ?: throw IllegalArgumentException("non-root should have parent")
+        val parent = newNode?.let { findParent(it) }
+        if (parent == null) {
+            return
+        }
 
         newNode.parent = parent
-
-        println("parent of ${newNode.key} is ${newNode.parent?.key}")
-
-        balanceAfterInsert(newNode)
-
         fixRedRedViolation(newNode)
         fixBlackBlackViolation(newNode)
+        balanceAfterInsert(newNode)
     }
-
     private fun getSibling(node: RBNode<K, V>): RBNode<K, V>? {
         if (node.parent == null) {
             return null
@@ -157,59 +140,15 @@ class RBTree<K : Comparable<K>, V>: balancedTree<K, V, RBNode<K, V>>() {
         }
     }
 
-
-    override fun rotateRight(node: RBNode<K, V>, parentNode: RBNode<K, V>?) {
-        val tempNode = node.leftChild ?: throw IllegalArgumentException("Node must have left child for right rotation")
-        node.leftChild = tempNode.rightChild
-        tempNode.rightChild = node
-        if (tempNode == root) {
-            tempNode.leftChild?.color = Color.BLACK
+    private fun changeColor(node: RBNode<K, V>): RBNode<K, V> {
+        if (node.color == Color.BLACK ){
+            node.color = Color.RED
+        } else {
+            node.color = Color.BLACK
         }
-    }
-    override fun rotateLeft(node: RBNode<K, V>, parentNode: RBNode<K, V>?) {
-        val tempNode = node.rightChild ?: throw IllegalArgumentException("Node must have right child for left rotation")
-        node.rightChild = tempNode.leftChild
-        tempNode.leftChild = node
-        if (tempNode == root) {
-            tempNode.rightChild?.color = Color.BLACK
-        }
+        return node
     }
 
-//    private fun rotateLeft(node: RBNode<K, V>?) {
-//        val newRoot = node?.rightChild ?: return
-//        node.rightChild = newRoot.leftChild
-//        if (newRoot.leftChild != null) {
-//            newRoot.leftChild?.parent = node
-//        }
-//        newRoot.parent = node.parent
-//        if (node.parent == null) {
-//            root = newRoot
-//        } else if (node == node.parent?.leftChild) {
-//            node.parent?.leftChild = newRoot
-//        } else {
-//            node.parent?.rightChild = newRoot
-//        }
-//        newRoot.leftChild = node
-//        node.parent = newRoot
-//    }
-
-//    override fun rotateRight(node: RBNode<K, V>, parentNode:  RBNode<K, V>?) {
-//        val newRoot = node?.leftChild ?: return
-//        node.rightChild = newRoot.leftChild
-//        if (newRoot.rightChild != null) {
-//            newRoot.rightChild?.parent = node
-//        }
-//        newRoot.parent = node.parent
-//        if (node.parent == null) {
-//            root = newRoot
-//        } else if (node == node.parent?.rightChild) {
-//            node.parent?.rightChild = newRoot
-//        } else {
-//            node.parent?.leftChild = newRoot
-//        }
-//        newRoot.rightChild = node
-//        node.parent = newRoot
-//    }
     private fun getUncle(node: RBNode<K, V>): RBNode<K, V>? {
         val parent = node.parent
         val grandparent = parent?.parent
@@ -224,138 +163,133 @@ class RBTree<K : Comparable<K>, V>: balancedTree<K, V, RBNode<K, V>>() {
         var currentNode = curNode
         while (currentNode.parent?.color == Color.RED) {
             var parent = currentNode.parent
-
-            val grandparent = parent?.parent
-
-            if (parent == grandparent?.leftChild) {
-                val uncle = grandparent?.rightChild
-
-                if (uncle?.color == Color.RED) {
-                    parent?.color = Color.BLACK
-                    uncle.color = Color.BLACK
-                    grandparent.color = Color.RED
-                    currentNode = grandparent
-                } else {
-                    if (currentNode == parent?.rightChild) {
-                        rotateLeft(currentNode, parent)
-                        currentNode = parent
-                        parent = currentNode.parent
-                    }
-
-                    parent?.color = Color.BLACK
-                    grandparent?.color = Color.RED
-                    rotateRight(currentNode, grandparent)
-                }
-            } else {
-                val uncle = grandparent?.leftChild
+                ?: throw IllegalStateException("parent can not be null")
+            var grandparent = parent.parent
+                ?: throw IllegalStateException("grandparent can not be null")
+            if (parent == grandparent.leftChild) {
+                var uncle = grandparent.rightChild
 
                 if (uncle?.color == Color.RED) {
-                    parent?.color = Color.BLACK
-                    uncle.color = Color.BLACK
-                    grandparent.color = Color.RED
-                    currentNode = grandparent
+                    parent = changeColor(parent)
+                    grandparent = changeColor(grandparent)
+                    uncle = changeColor(uncle)
                 } else {
-                    if (currentNode == parent?.leftChild) {
-                        rotateRight(currentNode, parent)
-                        currentNode = parent
-                        parent = currentNode.parent
+                    if (parent == grandparent.leftChild) {
+                        if (currentNode == parent.rightChild) rotateLeft(parent, grandparent)
+                        rotateRight(grandparent, grandparent.parent)
+                        currentNode = grandparent.leftChild!! //SORRYYYYYY
+                        currentNode.rightChild?.let { changeColor(it) }
+                    } else {
+                        if (currentNode == parent.leftChild) rotateLeft(parent, grandparent)
+                        rotateLeft(grandparent, grandparent.parent)
+                        currentNode = grandparent.rightChild!! //SORRYYYYYY
+                        currentNode.leftChild?.let { changeColor(it) }
                     }
-
-                    parent?.color = Color.BLACK
-                    grandparent?.color = Color.RED
-                    rotateLeft(currentNode, grandparent)
+                    currentNode = changeColor(currentNode)
+                    break
                 }
+            }
+            if (currentNode.parent == null && currentNode.color == Color.RED){
+                currentNode = changeColor(currentNode)
+            }
+            while (currentNode.parent != null) {
+                currentNode = currentNode.parent
+                    ?: throw IllegalStateException("currentNode must have parent")
             }
         }
     }
 
 
     override fun balanceAfterDelete(curNode: RBNode<K, V>) {
-        var parent = curNode.parent
+        var currentNode = curNode
         // Пока текущий узел не явл корнем и он черный
-        while (curNode != parent && curNode.color == Color.BLACK && parent != null) {
-            var sibling = getSibling(curNode)
-
-            if (curNode == parent.leftChild) {
-                if (sibling != null && parent.color == Color.BLACK)// Текущий узел находится слева от родителя
-                    if (sibling.color == Color.RED) {
-                        sibling.color = Color.BLACK
-                        parent.color = Color.RED
-                        rotateLeft(curNode, curNode.parent)
-                        sibling = getSibling(curNode)
-                }
-
-                if (sibling != null && sibling.leftChild?.color == Color.BLACK && sibling.rightChild?.color == Color.BLACK) { // Случай 2: Брат и соседняя вершина брата черные
-                    sibling.color = Color.RED
-                    curNode.parent = parent
-                } else {
-                    if (sibling?.rightChild?.color == Color.BLACK) { // Случай 3: Соседняя вершина брата черная
-                        sibling.leftChild?.color = Color.BLACK
-                        sibling.color = Color.RED
-                        rotateRight(curNode, sibling)
-                        sibling = getSibling(curNode)
-                    } else { // Случай 4: Соседняя вершина брата красная
-                        sibling?.leftChild?.color = Color.RED
-                        sibling?.color = Color.BLACK
-                        if (curNode != root) {
-                            rotateRight(curNode, sibling)
-                            sibling = getSibling(curNode)
+        while (currentNode.parent != null && currentNode.color == Color.BLACK) {
+            var parent = currentNode.parent
+                ?: throw IllegalStateException("parent can not be null")
+            if (currentNode == parent.leftChild) {
+                var right = parent.rightChild
+                    ?: throw IllegalStateException("parent must have right child")
+                if (parent.color == Color.RED){
+                    if (right.leftChild?.color == Color.RED || right.rightChild?.color == Color.RED){
+                        parent = changeColor(parent)
+                        if (right.leftChild?.color == Color.RED) {
+                            rotateRight(right, right.parent)
+                        } else {
+                            right = changeColor(right)
+                            right.rightChild = right.rightChild?.let { changeColor(it) }
                         }
+                        rotateLeft(parent, parent.parent)
+                        currentNode = parent.rightChild!!
+                        break
+                    } else { //rightChild has not any red children
+                        parent = changeColor(parent)
+                        right = changeColor(right)
                     }
-                }
-            } else { //текущий узел справа от родителя
-                if (sibling?.color == Color.RED) { 
-                    sibling.color = parent.color
-                    parent.color = Color.BLACK
-                    sibling.leftChild?.color = Color.BLACK
-                    rotateRight(curNode, getSibling(curNode))
-                    sibling = getSibling(curNode)
+                    break
+                } else{
+
                 }
 
-                if (sibling?.rightChild?.color == Color.BLACK && sibling.leftChild?.color == Color.BLACK) { // Случай 2: Брат и соседняя вершина брата черные
-                    sibling.color = Color.RED
-                    curNode.parent = parent
-                    parent = curNode.parent
-                } else {
-                    if (sibling?.leftChild?.color == Color.BLACK) { // Случай 3: Соседняя вершина брата черная
-                        sibling.rightChild?.color = Color.BLACK
-                        sibling.color = Color.RED
-                        rotateLeft(curNode, sibling)// meow
-                        parent = curNode.parent
-                    } else {
-                        sibling?.color = parent.color
-                        parent.color = Color.BLACK
-                        sibling?.leftChild?.color = Color.BLACK
-                         if (curNode != root) {
-                             curNode.parent?.let { rotateRight(it, sibling) }
-                             parent = curNode.parent
-                         }
-                    }
-                }
+//                if (sibling != null && sibling.leftChild?.color == Color.BLACK && sibling.rightChild?.color == Color.BLACK) { // Случай 2: Брат и соседняя вершина брата черные
+//                    sibling.color = Color.RED
+//                    curNode.parent = parent
+//                } else {
+//                    if (sibling?.rightChild?.color == Color.BLACK) { // Случай 3: Соседняя вершина брата черная
+//                        sibling.leftChild?.color = Color.BLACK
+//                        sibling.color = Color.RED
+//                        rotateRight(curNode, sibling)
+//                        sibling = getSibling(curNode)
+//                    } else { // Случай 4: Соседняя вершина брата красная
+//                        sibling?.leftChild?.color = Color.RED
+//                        sibling?.color = Color.BLACK
+//                        if (curNode != root) {
+//                            rotateRight(curNode, sibling)
+//                            sibling = getSibling(curNode)
+//                        }
+//                    }
+//                }
+//            } else { //текущий узел справа от родителя
+//                if (sibling?.color == Color.RED) {
+//                    sibling.color = parent.color
+//                    parent.color = Color.BLACK
+//                    sibling.leftChild?.color = Color.BLACK
+//                    rotateRight(curNode, getSibling(curNode))
+//                    sibling = getSibling(curNode)
+//                }
+//
+//                if (sibling?.rightChild?.color == Color.BLACK && sibling.leftChild?.color == Color.BLACK) { // Случай 2: Брат и соседняя вершина брата черные
+//                    sibling.color = Color.RED
+//                    curNode.parent = parent
+//                    parent = curNode.parent
+//                } else {
+//                    if (sibling?.leftChild?.color == Color.BLACK) { // Случай 3: Соседняя вершина брата черная
+//                        sibling.rightChild?.color = Color.BLACK
+//                        sibling.color = Color.RED
+//                        rotateLeft(curNode, sibling)// meow
+//                        parent = curNode.parent
+//                    } else {
+//                        sibling?.color = parent.color
+//                        parent.color = Color.BLACK
+//                        sibling?.leftChild?.color = Color.BLACK
+//                         if (curNode != root) {
+//                             curNode.parent?.let { rotateRight(it, sibling) }
+//                             parent = curNode.parent
+//                         }
+//                    }
             }
-            curNode.color = Color.BLACK
         }
-    }
-    override fun delete(key: K) {
-        val parent = deleteNode(key)
-        parent?.let {
-            balanceAfterDelete(parent)
-            //fixRedRedViolation(parent)
-            //fixBlackBlackViolation(parent)
-        }
+        curNode.color = Color.BLACK
     }
 }
 
-fun main() {
-    var rb = RBTree<Int, Any>()
-    rb.insert(4, 1)
-    rb.insert(5, 1)
-    rb.insert(3, 1)
-    rb.insert(2, 1)
-    rb.delete(5)
-    //rb.insert(7, 10)
 
-    val myList = rb.preorderTraversal()
+fun main(){
+    val tree = RBTree<Int, Any>()
+    tree.insert(7, "hi")
+    tree.insert(11, "hi")
+    tree.insert(5, "hi")
+    tree.insert(12, "hi")
+    val myList = tree.preorderTraversal()
     for (item in myList) {
         print("$item ")
     }
